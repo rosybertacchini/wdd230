6c58992d7d916daf3e6a18fd5cf7b0414f2ad5e4
rosybertacchini <rosybertacchini@gmail.com>
2024-02-03 11:59:36 -0800

    W04-2

---
+++ chamber/scripts/base.js
@@ -0,0 +1,37 @@
+// Chamber of Commerce screen
+
+
+/* screen */
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+
+
+/* Menu for mobile devices */
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
+
+
+const getWeatherInfo = async () => {
+	const response = await fetch(	  
+	  "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=bf0dd84c28e62391c7294fb52233ed70"
+	).then((response) => response.json());
+	console.log(response);
+  
+	// // cearte an array
+	// weatherInfo = response.record.coords;
+	// console.log(weatherInfo);
+  };
+
+
+
+
+  // *********************************************************** //
+  getWeatherInfo();
---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,12 @@
+  const options = {
+    weekly: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  };
+  document.getElementById("lastModified").textContent =
+    "Last Modified: " + new Date().toLocaleDateString("en-US", options);
+  document.getElementById("year").textContent = new Date().getFullYear();
+
+
+  
