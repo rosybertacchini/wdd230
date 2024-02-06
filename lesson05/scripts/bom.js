28f20c705a9c897bd430f85b088da3f5014a196d
rosybertacchini <rosybertacchini@gmail.com>
2024-02-06 15:36:01 -0800

    book of Mormon - corrections

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -6,22 +6,34 @@ const list = document.querySelector("#list")
 button.addEventListener("click", function () {
     if (input.value != '')  {
             let li = document.createElement("li");
-            let deleteButton = document.createElement("button");
+
         
+            let deleteButton = document.createElement("button");
+
             li.textContent = input.value;
-            deleteButton.textContent = "Delate Chapter";
+            /*  **************** */
+            deleteButton.style.color = "red";
+            deleteButton.textContent = " X ";
+            deleteButton.style.width = '10%';
 
             deleteButton = deleteButton;
+
+            /*  **************** */
+
+
             li.append(deleteButton);
             list.append(li);
             deleteButton.addEventListener("click", function () {
                 list.removeChild(li);
                 input.focus();
+                lis();
         });
     input.focus();
     input.value = "";
+    
     }
-});
+ });
+
 
 // ó se puede declarar como una funcion arrow
 // button.addEventListener("click", () => {  });
