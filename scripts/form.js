120ba14cd92d30fc4ece08a864e91c505142c256
rosybertacchini <rosybertacchini@gmail.com>
2024-02-27 13:57:38 -0800

    W08-built form

---
+++ scripts/form.js
@@ -0,0 +1,24 @@
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("range");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
+
+
+/* *************** VALIDETE EMAIL ******************* */
+
+function validateEmail() {
+    var emailInput = document.getElementById('email');
+    var emailPattern = /^[a-zA-Z0-9._%+-]+@byui\.edu$/;
+
+    if (!emailPattern.test(emailInput.value)) {
+        alert('Please enter a valid BYU-Idaho email address.');
+    }
+}
+
