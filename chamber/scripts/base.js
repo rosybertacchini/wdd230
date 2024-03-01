249e8fcfbd5861b26467dcdcafbb93d930f84c88
rosybertacchini <rosybertacchini@gmail.com>
2024-02-29 21:41:46 -0800

    W08-last correction

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -34,10 +34,4 @@ modeButton.addEventListener("click", () => {
 	}
 });
 
-// **********************************************
-
-document.addEventListener("DOMContentLoaded", function() {
-	// When the document is ready, set the value of the hidden input to the current timestamp
-	document.getElementById("timestampInput").value = Date.now();
-});
 
---
+++ chamber/scripts/date.js
@@ -0,0 +1,7 @@
+
+// **********************************************
+
+document.addEventListener("DOMContentLoaded", function() {
+	// When the document is ready, set the value of the hidden input to the current timestamp
+	document.getElementById("timestampInput").value = Date.now();
+});
