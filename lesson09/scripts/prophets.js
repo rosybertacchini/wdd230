44e82d87911eb8dd5f55cc98e7558cdd5b52c6ff
rosybertacchini <rosybertacchini@gmail.com>
2024-03-06 16:06:15 -0800

    W09

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,65 @@
+const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+
+
+
+
+}
+
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+
+        // 1. Create a section element
+        let card = document.createElement('section');
+        // 2. Create an h2 element
+        let fullName = document.createElement('h2');
+        // 3. Create an img element
+        let portrait = document.createElement('img');
+        
+        // datails for mobile
+        let divDetails = document.createElement('div');
+        let divDateBirth = document.createElement('span');
+        let divPlaceBirth = document.createElement('span');
+
+        // 4. Populate the heading element with the prophet's full name
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        
+        // 5. Build the image element
+        portrait.src = `${prophet.imageurl}`;
+        portrait.setAttribute('alt', `${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '300');
+        portrait.setAttribute('height', '400');
+        // prepare Details for Mobile 
+        divDateBirth.innerHTML = "Date of Birth: " + `${prophet.birthdate}`;
+        divPlaceBirth.innerHTML = "Place of Birth: " + `${prophet.birthplace}`;
+
+        // 6. AppendChild() on the section element named "card"
+        divDetails.appendChild(divDateBirth);
+        divDetails.appendChild(divPlaceBirth);
+   
+        card.appendChild(fullName); 
+        card.appendChild(divDetails);        
+        card.appendChild(portrait);
+        cards.appendChild(card);
+
+        // 7. Add the section card to the "cards" div
+        const cardsContainer = document.getElementById('cards');
+        cardsContainer.appendChild(card);
+
+    });
+}
+
+
+getProphetData();
+
+
+
---
+++ scripts/links.js
@@ -0,0 +1,45 @@
+
+const baseURL = "https://rosybertacchini.github.io/wdd230/";
+const linksURL  = "../data/links.json";
+//const linksURL  = "https://rosybertacchini.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+  }
+  
+const ol = document.querySelector("#activities");
+
+function displayLinks(weeks) {
+    weeks.lessons.forEach(lesson => {
+        // create an li element for element ol
+        const li = document.createElement('li');
+       
+        console.log(`${lesson.lesson}`);
+        const m_lessonNo = `${lesson.lesson}`;
+        lesson.links.forEach(link => {
+            console.log(`${link.url}`);
+            console.log(`${link.title}`);
+            // build a element
+            const a = document.createElement("a");
+            a.href = `${link.url}`;
+            a.innerHTML = `${link.title}` + " | ";
+            li.appendChild(a);
+            ol.appendChild(li);
+    
+        })
+
+    })
+}
+
+
+
+
+
+
+
+
+
+
+  getLinks();
