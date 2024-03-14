cb1250fec0daee370b1021b17d55c6f52949c4f9
rosybertacchini <rosybertacchini@gmail.com>
2024-03-14 16:06:58 -0700

    w10

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,5 +1,7 @@
 const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
+//const weatherIcon = document.querySelector('#weather-icon');
+const weatherIcon = document.createElement('img');
+const figure = document.querySelector('figure');
 const captionDesc = document.querySelector('figcaption');
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=32.50310852801206&lon=-117.00470232465491&appid=bf0dd84c28e62391c7294fb52233ed70&units=metric";
@@ -26,9 +28,10 @@ const displayResults = (data)  => {
         currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;C`;  // caracter especial para grados
         const iconsrc = `https://openweathermap.org/img/w/${w.icon}`;
         
-  calculateWindChill(`${data.main.temp}`, `${data.wind.speed}`);
+        calculateWindChill(`${data.main.temp}`, `${data.wind.speed}`);
         weatherIcon.setAttribute('src', iconsrc+ ".png");
         weatherIcon.setAttribute('alt',"Weather Icon");
+        figure.appendChild( weatherIcon);
         captionDesc.textContent = capitalizeEachWord(`${w.description}`);
     });
 
@@ -71,50 +74,3 @@ apiFetch()
 
 
 
-
-
-
-
-
-
-
-// const currentTemp = document.querySelector('#current-temp');
-// const weatherIcon = document.querySelector('#weather-icon');
-// const captionDesc = document.querySelector('figcaption');
-
-// const url = "https://api.openweathermap.org/data/2.5/weather?lat=32.50310852801206&lon=-117.00470232465491&appid=bf0dd84c28e62391c7294fb52233ed70&units=metric";
-
-
-// async function apiFetch() {
-//     try {
-//         const response = await fetch(url);
-//         if (response.ok) {
-//             const data = await response.json();
-//             console.table(data); 
-//             displayResults(data);
-//         } else {
-//             throw Error(await response.text());
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// const displayResults = (data)  => {
-
-//     data.weather.forEach(w => {   // can be more then one eather event
-//         currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;C`;  // caracter especial para grados
-//         const iconsrc = `https://openweathermap.org/img/w/${w.icon}`;
-//         weatherIcon.setAttribute('src', iconsrc+ ".png");
-//         weatherIcon.setAttribute('alt',"Weather Icon");
-//         captionDesc.textContent = capitalizeEachWord(`${w.description}`);
-//     });
-
-// }
-
-// function capitalizeEachWord(str) {
-//     // Split the string into an array of words, capitalize each word, and join them back
-//     return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-//   }
-  
-apiFetch()
