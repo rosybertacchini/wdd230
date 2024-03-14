0799329cda7b76ee88194f38231a7558db09cc92
rosybertacchini <rosybertacchini@gmail.com>
2024-03-14 16:17:27 -0700

    w10

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,5 +1,5 @@
 const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
+const icon = document.querySelector('#icon');
 const captionDesc = document.querySelector('figcaption');
 
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=32.50310852801206&lon=-117.00470232465491&appid=bf0dd84c28e62391c7294fb52233ed70&units=metric";
@@ -24,10 +24,17 @@ const displayResults = (data)  => {
 
     data.weather.forEach(w => {   // can be more then one eather event
         currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;C`;  // caracter especial para grados
+        calculateWindChill(`${data.main.temp}`, `${data.wind.speed}`);
         const iconsrc = `https://openweathermap.org/img/w/${w.icon}`;
-        weatherIcon.setAttribute('src', iconsrc+ ".png");
-        weatherIcon.setAttribute('alt',"Weather Icon");
-        captionDesc.textContent = capitalizeEachWord(`${w.description}`);
+        const img = document.createElement('img');
+
+        img.setAttribute('src', iconsrc + ".png");
+        img.setAttribute('alt',"Weather Icon");
+
+        icon.appendChild(img);
+
+
+        captionDesc.textContent = capitalizeEachWord(`${w.description}`) ;
     });
 
 }
@@ -37,4 +44,25 @@ function capitalizeEachWord(str) {
     return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
   }
   
+ // **************************** calculate WIND CHILL  *************************************** 
+
+function calculateWindChill(temperature, windSpeed){
+
+    const result =  document.getElementById('result');
+    var windChill = "";
+
+    if( temperature <= 10  ||  windChill >= 4.82) {
+        var windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+        result.innerText = "Wind Chill Factor: " + windChill.toFixed(2) + "°C";
+    } else {
+        result.innerText = "There is no conditions to have wind chill";
+        result.style.color = "red";
+    }
+}       
+// **************************** calculate WIND CHILL END  ***************************************
+
+
+
+
+
 apiFetch()
