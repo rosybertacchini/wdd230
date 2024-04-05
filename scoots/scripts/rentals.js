2430bc77c96a4fc7cf0786a7b864cbfee968bd97
rosybertacchini <rosybertacchini@gmail.com>
2024-04-05 11:23:48 -0700

    scoots

--- scoots/scripts/rentals.js
+++ scoots/scripts/rentals.js
@@ -12,7 +12,7 @@ async function getSootData() {
 
 function displayScooterTable(scooters) {
     const table = document.getElementById("scooterTable");
-    
+
 
     // Clear existing table content
     table.innerHTML = "";
@@ -36,7 +36,7 @@ function displayScooterTable(scooters) {
         });
         table.appendChild(headerRow);
 
-    
+
 
         // Populate table with scooter information
         inventory.forEach(scooter => {
@@ -56,10 +56,10 @@ function displayScooterTable(scooters) {
             const img = document.createElement("img");
             img.src = scooter.imgPath;
             img.alt = scooter.title;
-            img.setAttribute('width', '90%');
+            img.setAttribute('width', '100%');
             img.setAttribute('height', '100%');
-           
-            
+
+
 
             imgCell.appendChild(img);
             row.insertBefore(imgCell, row.firstChild);
@@ -81,7 +81,7 @@ const displayScooterTableForMobile = (scooters) => {
         const category = document.createElement('h2');
         category.innerHTML = `${scooter.category}`;
         category.setAttribute('id' , 'category')
-        section_cards.appendChild(category); 
+        section_cards.appendChild(category);
 
         const cards = document.createElement('div');
         cards.setAttribute('class', 'cards');
@@ -101,15 +101,56 @@ const displayScooterTableForMobile = (scooters) => {
             card_img.setAttribute('class', 'card-img');
             card_img.src = item.imgPath;
             card_img.alt = item.title;
+            // ---------------------------------------------------- //
+
+            const card_descriptionDiv = document.createElement('div');
+            card_descriptionDiv.setAttribute('id', 'card-descriptionDiv');
 
             const card_description = document.createElement('p');
+            const card_max_persons = document.createElement('p');
+            const card_half_day_res = document.createElement('p');
+            const card_full_day_res = document.createElement('p');
+            const card_half_day_w_in = document.createElement('p');
+            const card_full_day_w_in = document.createElement('p');
+
+
+
+
+            
             card_description.setAttribute('class', 'card-description');
+            card_max_persons.setAttribute('class', 'card-description');
+            card_half_day_res .setAttribute('class', 'card-description');
+            card_full_day_res.setAttribute('class', 'card-description');
+            card_half_day_w_in.setAttribute('class', 'card-description');
+            card_full_day_w_in.setAttribute('class', 'card-description');
+
+            console.log(item)
+                console.log(item.max_persons)
             card_description.textContent = item.desc;
+            card_max_persons.textContent = item.max-persons;
+            card_half_day_res.textContent = item.full_day_res;
+            card_full_day_res.textContent = item.full_day_res;
+            card_half_day_w_in.textContent = item.half_day_w_in;
+            card_full_day_w_in.textContent = item.full_day_w_in;  
+
+
+
+
+
+
+            card_descriptionDiv.appendChild(card_description)
+            card_descriptionDiv.appendChild(card_max_persons);
+            card_descriptionDiv.appendChild(card_half_day_res);
+            card_descriptionDiv.appendChild(card_full_day_res);
+            card_descriptionDiv.appendChild(card_half_day_w_in);
+            card_descriptionDiv.appendChild(card_full_day_w_in);
+
+            card.appendChild(card_descriptionDiv);
+       
 
             card.appendChild(card_title);
             card.appendChild(card_img);
-            card.appendChild(card_description);
-
+            card.appendChild(card_descriptionDiv);
             cards.appendChild(card);
         });
 
